b3c30f18d419214a5f65adcfffa81363a0ae4feb
jvabbott98 <97220454+jvabbott98@users.noreply.github.com>
2024-03-31 14:38:33 +0900

    added api integration

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,31 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Inception",
-            image: "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
-            director: "Christopher Nolan"
-        },
-        {
-            id: 2,
-            title: "Jaws",
-            image: "https://upload.wikimedia.org/wikipedia/commons/9/93/Jaws_movie_poster.png",
-            director: "Steven Spielberg"
-        },
-        {
-            id: 3,
-            title: "Zoolander",
-            image: "https://upload.wikimedia.org/wikipedia/en/7/7c/Movie_poster_zoolander.jpg",
-            director: "Ben Stiller"
-        }
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("https://justinsmoviedb-6d40ef42c02f.herokuapp.com/movies")
+          .then((response) => response.json())
+          .then((data) => {
+            const moviesFromApi = data.map((movie) => {
+              return {
+                title: movie.title,
+                image: movie.imageUrl,
+                director: movie.director.name,
+                description: movie.description,
+                genre: movie.genre.name
+              };
+            });
     
+            setMovies(moviesFromApi);
+          });
+      }, []);
+
     if (selectedMovie) {
         return (
             <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
